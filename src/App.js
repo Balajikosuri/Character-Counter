ed4b73b63a78f8273eca1ddd5525ca93945340c0
balajikosuri <balajikosuri2@gmail.com>
2023-09-23 08:44:34 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+import CharacterCounter from './components/CharacterCounter'
+
+// Replace your code here
+const App = () => (
+  <div className="app">
+    <div className="responsive">
+      <CharacterCounter />
+    </div>
+  </div>
+)
+
+export default App
---
+++ src/components/CharacterCounter/index.js
@@ -0,0 +1,98 @@
+import './index.css'
+import {Component} from 'react'
+
+import {v4 as uuidV4} from 'uuid'
+
+const test = {
+  id: uuidV4(),
+  sentence: 'balaji Kosuri',
+}
+
+class CharacterCounter extends Component {
+  state = {
+    CharacterList: [],
+    inputValue: '',
+  }
+
+  onCLickAddBtn = () => {
+    const {inputValue, CharacterList} = this.state
+    const listItem = {
+      id: uuidV4(),
+      sentence: inputValue,
+    }
+
+    if (inputValue !== '') {
+      CharacterList.push(listItem)
+      this.setState({inputValue: ''})
+    }
+  }
+
+  onChangeInput = e => this.setState({inputValue: e.target.value})
+
+  renderNoUserSection = () => (
+    <div className="no-user">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/no-user-inputs-img.png "
+        alt="no user inputs"
+      />
+    </div>
+  )
+
+  render() {
+    const {CharacterList, inputValue} = this.state
+    console.log(CharacterList)
+
+    return (
+      <div className="CharacterCounter">
+        <div className="char-count">
+          <div className="char-count-header">
+            <h1 className="char-count-heading">
+              Count the characters like a Boss...
+            </h1>
+          </div>
+          <div className="char-count-content">
+            {CharacterList.length !== 0 ? (
+              <ul className="chars-items">
+                {CharacterList.map(eachChar => (
+                  <li className="chars-item" key={eachChar.id}>
+                    <p className="character-its-count">
+                      {eachChar.sentence}: {eachChar.sentence.length}
+                    </p>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              this.renderNoUserSection()
+            )}
+          </div>
+        </div>
+        <div className="char-input">
+          <h1 className="char-input-heading">Character Counter</h1>
+          <form
+            onSubmit={e => {
+              e.preventDefault()
+            }}
+            className="input-section"
+          >
+            <input
+              value={inputValue}
+              onChange={e => this.onChangeInput(e)}
+              className="form-control user-input"
+              type="text"
+              placeholder="Enter the Characters here"
+            />
+            <button
+              onClick={() => this.onCLickAddBtn()}
+              className="add-btn"
+              type="button"
+            >
+              Add
+            </button>
+          </form>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default CharacterCounter
